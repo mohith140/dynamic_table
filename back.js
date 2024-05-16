25abe07713cab36bcdd0b45e500750efc8b14c00
mohith140 <118293990+mohith140@users.noreply.github.com>
2024-05-16 14:28:13 +0530

    Update back.js

--- back.js
+++ back.js
@@ -1,5 +1,6 @@
 var roll;var fname;var lname;var branch;
-function fetch(){
+
+function fetch1(){
      roll = document.getElementById("roll").value;
  fname = document.getElementById("fname").value;
      lname = document.getElementById("lname").value;
@@ -7,9 +8,18 @@ function fetch(){
 }
 function submit(){
    
-    fetch();
+    fetch1();
     var table = document.getElementById("table");
     var tr = document.createElement("tr");
+    fetch('http://localhost:3000/students',{method:'POST',body: JSON.stringify({
+      "id": `${roll}`,
+      "fname": `${fname}`,
+      "lname": `${lname}`,
+      "branch": `${branch}`
+    })})
+    .then((res)=>res.json())
+    .then((res)=>console.log(res))
+   
     tr.innerHTML = `
       <td>${roll}</td>
       <td>${fname}</td>
@@ -24,12 +34,29 @@ function submit(){
 }
 function Delete(this1){
     var row=this1.parentNode;
-    console.log(row)
+    console.log(this1.innerHTML+" "+this1.value+" "+this1.value)
+    fetch(`http://localhost:3000/students/${row.querySelector('td:first-child').textContent}`,{method:'DELETE'})
+    .then((res)=>res.json())
+    .then((res)=>console.log(res))
+   
     row.parentNode.removeChild(row);
 }
 function Edit(this1){
-    fetch();
+    fetch1();
     var row=this1.parentNode;
+    var patchData={}
+    if(roll!==''||roll!==undefined)
+    patchData['id']=roll
+    if(fname!==''||fname!==undefined)
+      patchData[fname]=fname
+    if(lname!==''||lname!==undefined)
+      patchData[lname]=lname
+    if(branch!==''||branch!==undefined)
+      patchData[branch]=branch
+    fetch(`http://localhost:3000/students/${row.querySelector('td:first-child').textContent}`,{method:'PATCH',body: JSON.stringify(patchData)})
+    .then((res)=>res.json())
+    .then((res)=>console.log(res))
+   
     row.innerHTML = `
     <td>${roll}</td>
     <td>${fname}</td>
@@ -37,4 +64,10 @@ function Edit(this1){
     <td>${branch}</td>
     <button onclick='Edit(this)'>Edit</button> <button onclick="Delete(this)">Delete </button>
   `;
+}
+function submit1(){
+   
+    fetch1();
+    axios.post("",{roll,fname,lname,branch})
+
 }
